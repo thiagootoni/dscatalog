c7093749821dbabddeb44f1bb00e949c0e471b85
Thiago Otoni <39462973+thiagootoni@users.noreply.github.com>
2021-02-07 11:56:21 -0300

    Update: Seed inicial do banco

--- backend/src/main/java/com/thiago/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/thiago/dscatalog/DscatalogApplication.java
@@ -1,13 +1,20 @@
 package com.thiago.dscatalog;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DscatalogApplication {
+public class DscatalogApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("No Ar!");
+		
 	}
 
 }
