8df9ef5354ab80b671f77ad3b6987e4fa8d5cd78
Thiago Otoni <39462973+thiagootoni@users.noreply.github.com>
2021-03-07 13:27:21 -0300

    Update UserUpdateDTO insert validation notation

--- backend/src/main/java/com/thiago/dscatalog/dto/UserUpdateDTO.java
+++ backend/src/main/java/com/thiago/dscatalog/dto/UserUpdateDTO.java
@@ -1,5 +1,8 @@
 package com.thiago.dscatalog.dto;
 
+import com.thiago.dscatalog.services.validations.UserUpdateValid;
+
+@UserUpdateValid
 public class UserUpdateDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 
