800afa39c6b6650c1508c09b1f41aa9d3c54008f
Thiago Otoni <39462973+thiagootoni@users.noreply.github.com>
2021-04-07 20:58:00 -0300

    Update Página de Detalhe do produto dinâmica

--- front-web/src/pages/Catalog/Components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/Components/ProductDetails/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { ReactComponent as ArrowIcon} from '../../../../core/assets/images/arrow.svg';
-import { ReactComponent as ProducImage} from '../../../../core/assets/images/product.svg'
 import ProductPrice from '../../../../core/components/ProductPrice';
+import { Product} from '../../../../core/types/Product';
+import makeRequest from '../../../../core/utils/request';
 import "./styles.scss";
 
 type ParamsType = {
@@ -14,6 +15,15 @@ const ProductDetails = () =>{
 
     const { productId } = useParams<ParamsType>();
 
+    const[product, setProduct] = useState<Product>();
+
+    useEffect(() => {
+
+        makeRequest({url: `/products/${productId}`})
+        .then(Response => setProduct(Response.data));
+
+    }, [productId]);
+
     return(        
         <div className="product-details-container card-base bd-radius-20">
            
@@ -22,26 +32,36 @@ const ProductDetails = () =>{
                 <h1 className="text-go-back">voltar</h1>
             </Link>
 
-            <div className="row">
-                <div className="col-6 pr-5">
-                    <div className = "card-product-details bd-radius-20 text-center">
-                        <ProducImage className="product-details-image"/>
-                    </div>
+            {
+                product && (
 
-                    <div className="product-details-info-container">
-                        <h1 className="product-name">Computador Desktop - Intel Core i7</h1>
-                        <ProductPrice price={2899} />
+                <div className="row">
+                    <div className="col-6 pr-5">
+                        <div className = "card-product-details bd-radius-20 text-center">
+                            <img src={product?.imgUrl} alt={product?.name} className="product-details-image"/>
+                        </div>
+
+                        <div className="product-details-info-container">
+                            <h1 className="product-name">
+                               {product?.name}
+                            </h1>
+                            <ProductPrice price={product?.price} />
+                        </div>
+                    </div>
+                
+                    <div className="col-6 card-product-details bd-radius-20">                    
+                        <h1 className="text-title-description">Descrição do Produto</h1>
+                        <p className="text-description">
+                            {
+                                product?.description
+                            }
+                        </p>                                      
                     </div>
                 </div>
-                
-                <div className="col-6 card-product-details bd-radius-20">                    
-                    <h1 className="text-title-description">Descrição do Produto</h1>
-                    <p className="text-description">
-                        Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.
-                    </p>                                      
-                </div>
-            </div>
-           
+
+                )
+            }
+
         </div>
     );   
 };
